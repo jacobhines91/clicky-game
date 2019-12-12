9c5caae5de23ec6442331c82368f6bdab532a98d
Jacob Hines <jacob_hines@hotmail.com>
2019-12-11 19:48:48 -0600

    Updates

---
+++ precache-manifest.707ef5cda8d654e8ad9a30505cacb212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84900504a2c8a8c3be9763647953e9c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0084c232812afe8e01c3",
+    "url": "/clicky-game/static/css/main.40126f58.chunk.css"
+  },
+  {
+    "revision": "12b17376e09af530068c",
+    "url": "/clicky-game/static/js/2.6ac3b5a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.6ac3b5a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0084c232812afe8e01c3",
+    "url": "/clicky-game/static/js/main.7a6e5e65.chunk.js"
+  },
+  {
+    "revision": "74b5bf453d1c1c8b010a",
+    "url": "/clicky-game/static/js/runtime-main.24a346c7.js"
+  }
+]);
